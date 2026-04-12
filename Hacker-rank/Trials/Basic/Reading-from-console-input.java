95dd3b0f5c2e03c8fdb777518ca6719c8bfcd505
Surya Pratap Singh <suryaprataps2024@gmail.com>
2026-04-12 22:37:14 +0530

    Add console input reading example in Java
    Implement console input reading and output formatting.

---
+++ Hacker-rank/Trials/Basic/Reading-from-console-input.java
@@ -0,0 +1,34 @@
+/*
+Sample Input
+
+Hello world
+I am a file
+Read me until end-of-file.
+
+
+Sample Output
+
+1 Hello world
+2 I am a file
+3 Read me until end-of-file.
+*/
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+       int num = 1;
+       while(sc.hasNextLine()){
+        System.out.println(num + " " +sc.nextLine());
+        num++;
+       }
+    }
+}
