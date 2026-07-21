44ff787ae5a0a31325bfcb31bf019c1b1dbc7ce8
Surya Pratap Singh <suryaprataps2024@gmail.com>
2026-07-21 22:42:52 +0530

    Add BasicDemo class with ArrayList example

---
+++ basics/arraylist.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class BasicDemo{
+	public static void main(String Args[]){
+		ArrayList<Double> d = new ArrayList<>();
+
+		d.add(90.4);
+		d.add(32.7);
+		d.add(78.5);
+		System.out.println(d);
+	}
+}
