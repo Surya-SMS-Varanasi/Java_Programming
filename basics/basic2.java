b8307fc8bec528d10b90af899bfbea4801da0dfa
Surya Pratap Singh <suryaprataps2024@gmail.com>
2026-07-22 22:28:29 +0530

    Create basic2.java

---
+++ basics/basic2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Tester{
+
+	public static void main(String args[]){
+		
+		ArrayList<String> amd = new ArrayList<>();
+
+		amd.add("Ballu"); 
+		amd.add("Dhannu");
+		amd.add("Tina");
+		amd.add("Bina");
+		amd.add("Mina");
+		System.out.println(amd);
+	}
+}
